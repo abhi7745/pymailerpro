daff22675ba541af359bbb9ea89b2fe0b0761c95
Abhijith KR <abhijithkr7745@gmail.com>
2024-06-07 21:06:29 +0530

    email sending logic added

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,27 @@
 
 import os, sys
 
+# email sending 
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+
+import random
+import time
+# Loading function
+def Loading(value=True):
+    loading = ['|||||', '/////', '\\\\\\']
+    # loading = ['🌟🌟🌟🌟🌟', '💫💫💫💫💫', '✨✨✨✨✨', '🌈🌈🌈🌈🌈', '🌸🌸🌸🌸🌸', '🌼🌼🌼🌼🌼', '🌞🌞🌞🌞🌞', '🌛🌛🌛🌛🌛', '🌟💫✨💫🌟']
+    
+    while value:
+        random.shuffle(loading)  # Shuffle the loading icons randomly
+        for i in loading:
+            # print(f'Please wait...{i}', end="\r")
+            print(f'\rPlease wait...{i}', end="")
+            time.sleep(0.1)
+        value = False
+
 EMAIL_PROVIDERS = ['@gmail.com', '@yahoo.com', '@outlook.com']  # Add other familiar email hosts here
 
 # For development purpose
@@ -23,6 +44,8 @@ def help():
     commands = f"\n-help, -h : Help command\n-reset, -r : Resetting your current email and password"
     
     print(commands)
+
+
 
 def config():
     while True:
@@ -59,7 +82,72 @@ def config():
                 # break
             break
 
+def get_email_subject_body():
+    def get_valid_email():
+         # To email valid
+        while True:
+            to_email = input("Enter recipient email: ").replace(' ', '')
 
+            if not to_email:
+                print('\nEmail recipient required!')
+                continue
+
+            if not any(to_email.endswith(domain) for domain in EMAIL_PROVIDERS):
+                print("\nInvalid recipient email. Supported email providers include 'gmail.com', 'yahoo.com', and 'outlook.com'.\nPlease try again.\n")
+                continue
+
+            return to_email
+        
+    def get_valid_subject():
+        while True:
+            subject = input("Enter email subject: ")
+
+            if not subject:
+                print("Subject required!")
+                continue
+            return subject
+        
+    def get_valid_message():
+         while True:
+            body = input("Enter email message: ")
+
+            if not body:
+                print("Message required!")
+                continue
+            return body
+
+
+    to_email = get_valid_email()
+    subject = get_valid_subject()
+    body = get_valid_message()
+
+
+    return to_email, subject, body
+
+
+
+
+def email_sender(to_email, subject, body):
+    # message setting area
+    message=EmailMessage()
+    message['From'] = EMAIL
+    message['To'] = to_email
+    message['Subject'] = subject
+    # message.set_content(body)
+
+    html = f"""
+            <h3>{body}</h3>
+    """
+
+    message.add_alternative(html, subtype="html")
+    context=ssl.create_default_context() # it securing connection
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(EMAIL, PASSWORD)
+        server.sendmail(EMAIL, to_email, message.as_string())
+
+    Loading()
+    print("\nYour email was sent successfully.")
 
 def main():
     if EMAIL == '' or PASSWORD == '':
@@ -86,7 +174,9 @@ def main():
     # print(len(sys.argv))
     
     else:
-        print('main code')
+        # print('main code')
+        to_email, subject, body = get_email_subject_body()
+        email_sender(to_email, subject, body)
     
 
 
